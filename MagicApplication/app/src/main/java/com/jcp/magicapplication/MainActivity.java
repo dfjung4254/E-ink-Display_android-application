d4460813e0ca0bc3404c00b6a27dc10be502748d
dfjung4254 <rmsghk4254@naver.com>
2019-08-28 17:36:00 +0900

    calendarTestFinished

--- MagicApplication/app/src/main/java/com/jcp/magicapplication/MainActivity.java
+++ MagicApplication/app/src/main/java/com/jcp/magicapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.jcp.magicapplication.Session.SessionJWT;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,6 +38,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtName;
+    private TextView txtCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +46,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtName = findViewById(R.id.MainActivity_TextView_Name);
+        txtCalendar = findViewById(R.id.MainActivity_TextView_Calendar);
 
-        new JSONPost(getResources().getString(R.string.server_url)+"/users/info").execute();
+        new JSONPost(JSONPost.GET_USER_INFO, null).execute();
+        new JSONPost(JSONPost.GET_CALENDAR, null).execute();
 
 
     }
@@ -62,17 +66,28 @@ public class MainActivity extends AppCompatActivity {
     private class JSONPost extends AsyncTask<String, JSONObject, JSONObject> {
 
         /* 멤버 변수 */
-        private String strUrl;              // 서버로 연결할 서버 URL
+        private String sJwt;                                                                // 서버로 요청할 JWT 인증값
+        private final String SERVER_URL = getResources().getString(R.string.server_url);                           // 서버로 연결할 서버 URL
+        private final String SERVER_DIR[] = {
+            "/users/info",                                                                  // GET_USER_INFO = 0
+            "/calendar/next"                                                                // GET_CALENDAR  = 1
+        };
+        private int option;                                                                 // 서버로 요청할 메서드
+        private JSONObject reqBody;
+        public static final int GET_USER_INFO = 0;                                          // 유저를 받아옴
+        public static final int GET_CALENDAR = 1;                                           // 달력 10일을 받아옴.
 
         /*
 
             생성자
-            서버로 보낼 URL 을 미리 받는다.
+            서버로 보낼 요청정보를 미리 받는다.
 
         */
-        public JSONPost(String strUrl){
+        public JSONPost(int option, JSONObject reqBody){
             super();
-            this.strUrl = strUrl;
+            this.option = option;
+            this.sJwt = SessionJWT.getInstance().getJwt();
+            this.reqBody = reqBody;
         }
 
         /*
@@ -93,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
                     JSON 객체를 받아 옴.
 
                 */
-                String sJwt = SessionJWT.getInstance().getJwt();
 
                 HttpURLConnection connection = null;
                 BufferedReader reader = null;
@@ -101,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 try{
 
                     /* 2번째 parameter 로 넘어온 URL 값으로 생성한 다음 해당 통신 오픈  */
-                    URL url = new URL(strUrl);
+                    URL url = new URL(SERVER_URL + SERVER_DIR[option]);
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("POST");                                // 전송 방식 : POST
                     connection.setRequestProperty("Cache-Control", "no-cache");         // 캐시 설정
@@ -116,7 +130,9 @@ public class MainActivity extends AppCompatActivity {
 
                     /* write 버퍼생성 및 내용 쓰기 */
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
-                    //writer.write(jsonObject.toString());
+                    if(reqBody != null){
+                        writer.write(reqBody.toString());
+                    }
                     writer.flush();
                     writer.close();
 
@@ -176,21 +192,43 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(JSONObject jsonObject) {
             super.onPostExecute(jsonObject);
 
-            /* UI 구성에 사용할 value 값 */
-            String txtChangeName = "no changed";
+            switch (option){
+                case GET_USER_INFO:
+                    /* UI 구성에 사용할 value 값 */
+                    String txtChangeName = "no changed";
 
-            /* jsonObject 에서 value 값을 추출한다. */
-            try {
+                    /* jsonObject 에서 value 값을 추출한다. */
+                    try {
 
-                txtChangeName = jsonObject.getString("email");
+                        txtChangeName = jsonObject.getString("email");
 
-                Log.d("****txtChangeName1 : ", txtChangeName);
+                        Log.d("****txtChangeName1 : ", txtChangeName);
 
-            } catch (Exception e) {
-                e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    txtName.setText(txtChangeName);
+                    break;
+                case GET_CALENDAR:
+                    Log.d("txtCalendarChange : ", jsonObject.toString());
+                    String txtCalendarChange = "no changed calendar";
+                    try{
+                        JSONArray arr = jsonObject.getJSONArray("days");
+                        txtCalendarChange = "";
+                        for(int i = 0; i < arr.length(); i++){
+                            txtCalendarChange += arr.getJSONObject(i).getString("summary");
+                            txtCalendarChange += '\n';
+                        }
+
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+
+                    txtCalendar.setText(txtCalendarChange);
+                    break;
+
             }
-
-            txtName.setText(txtChangeName);
 
         }
 
